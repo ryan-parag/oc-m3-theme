ad8d1e30d5bbf78c49f1bb8bf46a98b314e1e017
Ryan Parag <parag.ryan@gmail.com>
2025-02-13 21:03:14 -0500

    change syntax theme

--- components/Code/index.js
+++ components/Code/index.js
@@ -1,9 +1,9 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { duotoneSpace } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const Code = ({ children }) => {
   return(
-    <SyntaxHighlighter language="json" style={oneDark} showLineNumbers wrapLongLines>
+    <SyntaxHighlighter language="json" style={duotoneSpace} showLineNumbers wrapLongLines>
       {children}
     </SyntaxHighlighter>
   )
